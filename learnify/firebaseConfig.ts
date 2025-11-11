7a94ab2c0df6f4b503de0ee16f0f99f96d04092b
Swara Dalvi <swara632006@gmail.com>
2025-11-11 15:40:30 +0530

    Remove sensitive Firebase config values
    Cleared Firebase configuration values for security.

--- learnify/firebaseConfig.ts
+++ learnify/firebaseConfig.ts
@@ -7,13 +7,13 @@ import {
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCUVNw94Toz2H9qoYS8Gn6Sog9GxvBMyP4",
-  authDomain: "learnify-28859.firebaseapp.com",
-  projectId: "learnify-28859",
-  storageBucket: "learnify-28859.firebasestorage.app",
-  messagingSenderId: "459480628501",
-  appId: "1:459480628501:web:24e53cc3e8d48ef421bd1c",
-  measurementId: "G-6RE3HXJ804"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 let app;
 if (!getApps().length) {
